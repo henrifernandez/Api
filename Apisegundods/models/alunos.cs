30c2e493989d2889816f65954c04be209c34819a
henrifernandez <enzopower2018@gmail.com>
2025-03-17 08:29:39 -0300

    de_uva

--- Apisegundods/models/alunos.cs
+++ Apisegundods/models/alunos.cs
@@ -2,5 +2,14 @@
 {
     public class alunos
     {
+        public int id {  get; set; }  
+        public string name { get; set; }
+        public string RMA { get; set; }
+        public string pai { get; set; }
+        public string mae { get; set; }
+        public Escola Escola { get; set; }
+        public Professor Professor { get; set; }
+
+
     }
 }
